e63512de9bdab4153a4f01fe9c71883661b5c951
scotthanselman <scotthanselman@e5ec1f06-1a35-0410-8410-1963223b2c43>
2007-07-23 19:31:34 +0000

    Fixing a misspelling in the namespace.

--- websabelib/Properties/AssemblyInfo.cs
+++ websabelib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("websabelib")]
+[assembly: AssemblyTitle("wesabelib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("websabelib")]
+[assembly: AssemblyProduct("wesabelib")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
